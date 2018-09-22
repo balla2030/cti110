9b52ad2a3c8b598689ba7b45a897bd49bc2a9855
balla2030 <43505953+balla2030@users.noreply.github.com>
2018-09-22 18:48:16 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_AprilBall.py
@@ -0,0 +1,15 @@
+# Sales Prediction Exercise 2
+# 9/11/2018
+# CTI-110 P2T1-Sales Prediction
+# April Ball
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print(' The profit is $', format(profit, ',.2f'))
+
+
